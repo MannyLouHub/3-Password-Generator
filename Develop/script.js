ab3e0bee30bf4d292920559254ad476d349458aa
Manny Lou <mavkio1@gmail.com>
2020-07-14 14:48:06 -0400

    Created the Genpassword Fun

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+
+function generatePassword() {
+  //Password Vaules 
+  var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "_", "=", ",", "<", ">", ".", "?", "/", ";", ":", "[", "]", "{", "}"];
+  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+  
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  //Password Vaules 
-  var upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var lowerLetter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var specialChar = ["!","@","#","$","%","^"."&","*","(",")","-","+","_","=",",","<",">",".","?","/",";",":","'",'"',"[","]","{","}"];
-  var numbers = ["1","2","3","4","5","6","7","8","9","0"];
-
- 
+  generatePassword();
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
