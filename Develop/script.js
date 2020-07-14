09ffbc76730f4af8a6115324ebfc37759a169495
Manny Lou <mavkio1@gmail.com>
2020-07-14 15:19:54 -0400

    added if statements to check, the checked boxed.

--- Develop/script.js
+++ Develop/script.js
@@ -5,12 +5,61 @@ var generateBtn = document.querySelector("#generate");
 
 
 function generatePassword() {
-  //Password Vaules 
-  var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var lowerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "_", "=", ",", "<", ">", ".", "?", "/", ";", ":", "[", "]", "{", "}"];
-  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  
+  //Check the Checkedbox. 
+  var checkBox = document.getElementById('inlineCheckbox1');
+      console.log(checkBox.checked);
 
+  var checkBox2 = document.getElementById('inlineCheckbox2');
+      console.log(checkBox2.checked);
+  
+  var checkBox3 = document.getElementById('inlineCheckbox3');
+      console.log(checkBox3.checked);
+    
+  var checkBox4 = document.getElementById('inlineCheckbox4');
+      console.log(checkBox4.checked);
+  
+  //Password Vaules 
+    var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "_", "=", ",", "<", ">", ".", "?", "/", ";", ":", "[", "]", "{", "}"];
+    var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  
+
+  //Concat The arrays
+
+  if(checkBox.checked){
+    console.log("Yay!");
+   }
+  else{
+    console.log("Oh No!");
+  }
+  if(checkBox2.checked){
+    console.log("Yay!");
+  }
+  else{
+    console.log("Oh No!");
+  }
+  if(checkBox3.checked){
+    console.log("Yay!");
+   }
+  else{
+    console.log("Oh No!");
+  }
+  if(checkBox4.checked){
+    console.log("Yay!");
+  }
+  else{
+    console.log("Oh No!");
+  }
+
+
+console.log(upperLetters[0]);
+
+var passwordValue = upperLetters.concat(lowerLetters);
+
+console.log(passwordValue);
   
 }
 
@@ -20,8 +69,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  generatePassword();
 }
+
+
 
 
 // Add event listener to generate button
